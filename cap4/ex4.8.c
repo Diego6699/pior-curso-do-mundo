561e3da22a980514af4de0e26f4658192d86e21e
Diego Leonardo <diegoleonardo9966@gmail.com>
2024-05-15 14:43:50 -0300

    ex4.8.c

--- cap4/ex4.8.c
+++ cap4/ex4.8.c
@@ -1,12 +1,65 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main( void ){
-    
-    int *c = NULL;
+int main(void)
+{
+
+    int arr[5];
+
+    int size = (sizeof(arr) / sizeof(int));
+    bool par = false;
+    bool impar = false;
+    for (int i = 0; i < size; i++)
     {
-        int b = 12;
-        c = &b;
+        printf("array[%d]: ", i);
+        scanf("%d", &arr[i]);
+
+        if (arr[i] % 2 == 0)
+        {
+            par = true;
+        }
+
+        if (arr[i] % 2 > 0)
+        {
+            impar = true;
+        }
     }
-    printf("%d",*c);
+    // pares
+    if (par)
+    {
+        /* code */
+        printf("Numeros pares:");
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                printf(" %d", arr[i]);
+            }
+        }
+        printf(".\n");
+    }
+    else
+    {
+        printf("Numeros pares: nao ha.\n");
+    }
+
+    // impares
+    if (impar)
+    {
+        /* code */
+        printf("Numeros impares:");
+        for (int i = 0; i < size; i++)
+        {
+            if (!(arr[i] % 2 == 0))
+            {
+                printf(" %d", arr[i]);
+            }
+        }
+        printf(".");
+    }
+    else
+    {
+        printf("Numeros impares: nao ha.");
+    }
     return 0;
 }
